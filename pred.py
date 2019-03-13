e5c40827781623a5c1d9f90a765c8d3b56700928
Maximellerbach <38923282+Maximellerbach@users.noreply.github.com>
2019-03-13 11:09:21 +0100

    Add files via upload

---
+++ pred.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import h5py
+import time
+import os
+import math
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from keras.models import load_model, Input, Model
+from glob import glob
+
+model1 = load_model('upmodel1.h5')
+model2 = load_model('upmodel2.h5')
+
+model1.summary()
+model2.summary()
+
+dos = glob('..\\out\\*')
+i = 0
+for path in tqdm(dos):
+    try:
+        
+        imgo = cv2.imread(path)
+        i+=1
+
+        w,h = imgo.shape[1], imgo.shape[0]
+
+        total = w*h
+        maximum = 1500*1500
+        
+        if total>maximum: #if image is too big, divideimage in multiple little image to pred and reconstruct it later
+            ratio = maximum/total
+
+            nb = 2
+
+            while(ratio<0.50):
+                ratio*=2
+                nb*= 2
+
+            w = w//nb
+            h = h//nb
+
+             
+            comp_img = []
+
+            for xs in range(1,nb+1):
+                for ys in range(1,nb+1):
+                    img = imgo[int(h*ys-h):int(h*ys), int(w*xs-w):int(w*xs)]                
+
+                    pred = np.expand_dims(img,axis=0)
+
+                    x = model1.predict(pred/255)
+                    new_img = model2.predict(x)
+
+                    comp_img.append(new_img[0])
+            
+            comp_img = np.array(comp_img)
+            new_img = np.zeros((new_img.shape[1]*nb,new_img.shape[2]*nb,3))
+
+            h *= new_img.shape[0]//imgo.shape[0]
+            w *= new_img.shape[1]//imgo.shape[1]
+            n = 0
+
+            #reconstruct image
+            for xs in range(1,nb+1):
+                for ys in range(1,nb+1):
+                    new_img[h*ys-h :h*ys, w*xs-w :w*xs] = comp_img[n]
+                    n+=1                
+                    
+            cv2.imwrite('..\\data\\'+str(i)+'.jpg',new_img*255)
+
+
+        else: #if image is little enought just pred the whole image
+
+            pred = np.expand_dims(imgo,axis=0)
+
+            x = model1.predict(pred/255)
+            new_imgo = model2.predict(x)
+        
+            cv2.imwrite('..\\data\\'+str(i)+'.jpg',new_imgo[0]*255)
+
+    except:
+        print('error')
+
+        
+        
+
+
+
+
+
---
+++ train_model.py
@@ -0,0 +1,110 @@
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply
+from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import AveragePooling2D, Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D, AveragePooling2D
+from keras.models import Sequential, Model, load_model
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import numpy as np
+from glob import glob
+from tqdm import tqdm
+import cv2
+import time
+
+class model_object():
+    def __init__(self):
+
+        #create or load models
+        self.model1, self.model2 = self.create_model()
+
+        #self.model1 = load_model('debmodel1.h5')
+        #self.model2 = load_model('debmodel2.h5')
+
+
+
+    def create_model(self):
+        
+        ############ IMAGE DEBLURING ############ 
+        # I create 2 models to see data in between
+
+        model1 = Sequential()
+        model2 = Sequential()
+
+        #### model 1
+
+        model1.add(Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="same", input_shape=(None,None,3)))
+        model1.add(Activation("relu"))
+
+        model1.add(Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="same"))
+        model1.add(Activation("relu"))
+
+        model1.add(Conv2D(32, kernel_size=(3,3), strides=(1,1), padding="same"))
+        model1.add(Activation("relu"))
+
+        #### model 2
+
+        model2.add(Conv2DTranspose(32, kernel_size=(3,3), strides=(1,1), padding = "same", input_shape = (None,None,32)))
+        model2.add(Activation("relu"))  
+
+        model1.add(Conv2DTranspose(32, kernel_size=(3,3), strides=(1,1), padding="same"))
+        model1.add(Activation("relu"))
+
+        model2.add(Conv2D(3, kernel_size=(1,1), strides=(1,1), padding = "same"))
+        model2.add(Activation("sigmoid"))
+
+        return model1, model2
+
+
+    def train(self, epochs = 20, batch_size = 32, X_train = [], Y_train = []):
+        
+        #creating combined model according to the current shape of the dataset
+
+        img = Input(shape=(self.shape))
+        x = self.model1(img)
+        label = self.model2(x)
+
+        self.combined = Model(img,label)
+        self.combined.compile(loss='mse', optimizer=Adam())
+        
+        if epochs == None: #to train without epochs limit 
+            epoch = 0
+            while(True):
+                epoch+=1
+                print(epoch)
+
+                self.combined.fit(X_train, Y_train, batch_size=batch_size, epochs=1)
+                self.model1.save('debmodel1.h5')
+                self.model2.save('debmodel2.h5')
+
+        else: #to train with epochs limit
+            self.combined.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs)
+            self.model1.save('debmodel1.h5')
+            self.model2.save('debmodel2.h5')
+
+
+
+if __name__ == '__main__':
+
+    #create model
+    model = model_object()
+
+    #load dataset
+    dos = glob('*.npy')
+    print(len(dos))
+
+    #train model with every dataset 
+    for i in range(len(dos)//2):
+
+        X_train = np.load('dataset\\'+str(i)+'_upX_train.npy')
+        print('loaded X_train', i)
+        Y_train = np.load('dataset\\'+str(i)+'_upY_train.npy')
+        print('loaded Y_train', i)
+    
+        #train model
+        model.shape = X_train.shape[1:]
+        model.train(epochs=10, batch_size=1, X_train=X_train, Y_train=Y_train)
+
+
+    
+
+    
